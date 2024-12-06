6455f88059d64ac3b3cdf939b03c5aeb111aae1f
leoproducts <masanja.developer@gmail.com>
2024-12-06 17:56:56 +0300

    Update www

--- freight_logistics/redirect_to_www.py
+++ freight_logistics/redirect_to_www.py
@@ -1,15 +1,12 @@
 from django.http import HttpResponsePermanentRedirect
 
-class RedirectToWWWMiddleware:
-    """
-    Middleware to redirect non-www requests (e.g., sifongo.co.tz) to www (e.g., www.sifongo.co.tz).
-    """
+class EnforceWwwMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
         host = request.get_host()
-        # Redirect only if the host is non-www
-        if host == "sifongo.co.tz":
-            return HttpResponsePermanentRedirect(f"https://www.sifongo.co.tz{request.get_full_path()}")
+        if not host.startswith('www.'):
+            new_url = request.build_absolute_uri().replace(f'://{host}', f'://www.{host}')
+            return HttpResponsePermanentRedirect(new_url)
         return self.get_response(request)
